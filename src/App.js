fba8845397e34e5c2aa8cda6205cc12ace5a4a8c
Mateus Castro da Cunha <mateus.mc87@gmail.com>
2022-05-10 16:48:10 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {useState} from "react";
+import Panel from './Components/Panel/Panel'
+
+function App() {
+  const [red, setStateRed] = useState (10);
+  const [green, setStateGreen] = useState (50);
+  const [blue, setStateBlue] = useState (100);
+  return (
+    <>
+      <div className="App">
+        <h1 style={{"margin": "0px","background-color": "blue", "color": "white"}}>Projeto RGB | Exercicio React</h1>
+      </div>
+      <div style={{"display": "flex", 
+      "flex-direction": "column",
+      "position": "relative",
+      "max-width": "300px",
+      "margin-left": "10%"}}>
+        <Panel backGroundColor={{red: red, green: green, blue: blue}}></Panel>
+        <strong>Red: </strong>{red}
+          <input value={red} onChange={({target})=> setStateRed(parseInt(target.value))} type="range" min={0} max={255}></input>
+        <strong>Green: </strong>{green}
+          <input value={green} onChange={(target) => setStateGreen(parseInt(target.target.value))} type="range" min={0} max={255}></input>
+        <strong>Blue:</strong>{blue}
+          <input value={blue} onChange={({target})=>setStateBlue(parseInt(target.value))} type="range" min={0} max={255}></input>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Panel/Panel.js
@@ -0,0 +1,23 @@
+import './Panel.css'
+function Panel (backGroundColor) {
+    let string = "#e0d900"
+    function converteValoresEmCor (backGroundColor){
+        // string = "rgb("+ backGroundColor.backGroundColor.red+ ", " +backGroundColor.backGroundColor.green + ", " +  backGroundColor.backGroundColor.blue +")"
+        // console.log(string)
+        string = "rgb(153, 255, 51)"
+        
+    }
+
+    
+    return (
+        <>
+            <div style={{"backgroundColor": string}} className="preview">
+            </div>
+            <button onClick={()=> converteValoresEmCor(backGroundColor)}>Printa cor
+            </button>
+        </>
+    );
+}
+
+
+export default Panel;
